e36327a1ab9eed327a2ceb8b1d43e7ceee103a7f
Odai-SiliconValley <shimashimanomi0422@ezweb.ne.jp>
2020-05-12 10:48:10 +0900

    Revert "group_new_edit"
    This reverts commit 8b5bdfe70426bfc3a48dd86effa94c69e6eefd2b.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'groups#index'
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]
+  root "messages#index"
 end
