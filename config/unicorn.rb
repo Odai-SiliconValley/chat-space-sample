2c4875c6eb908d2ad23b9b8c5e9edaaf2833c397
Odai-SiliconValley <shimashimanomi0422@ezweb.ne.jp>
2020-05-21 17:09:12 +0900

    eee

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,13 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
+
+worker_processes 1
+
+working_directory app_path
+pid "#{app_path}/tmp/pids/unicorn.pid"
+listen "#{app_path}/tmp/sockets/unicorn.sock"
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
